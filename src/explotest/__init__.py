089c8871a9eb3ddf377a5a19ba2f32c87548dce1
Zihao Huang <zihaohuang2017@gmail.com>
2024-08-14 14:08:18 -0700

    Update readme

--- src/explotest/__init__.py
+++ src/explotest/__init__.py
@@ -3,6 +3,8 @@ from IPython.core.magic import register_line_magic
 
 from .explotest import transform_tests_wrapper
 
+__version__ = "0.1.5"
+
 
 def load_ipython_extension(ipython: IPython.InteractiveShell):
     register_line_magic(transform_tests_wrapper(ipython))
